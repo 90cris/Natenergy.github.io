cd9d2d54b78d410af9580f87e0c7fb81942a0317
cris90 <cgut90@hotmail.com>
2024-10-17 11:08:45 -0300

    correccion de variables2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://90cris.github.io/Natenergy",
+  base: "https://90cris.github.io/Natenergy.github.io",
 });
