494c962f8c68e19ec37acec80c8443c8109549a1
90cris <166570889+90cris@users.noreply.github.com>
2025-04-01 16:10:03 -0300

    Corrigiendo configuración para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://90cris.github.io/Natenergy.github.io",
+  base: '/Natenergy.github.sitio/',
 
 });
 
